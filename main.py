24c56c92d57508f10b9e557936f00a60985d4d10
Mlg012784 <mlg012784@yahoo.com>
2017-11-27 19:29:35 -0600

    user-signup templates made

--- main.py
+++ main.py
@@ -6,7 +6,37 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 
 @app.route("/")
+def display_form():
+    return render_template('index.html')
+
+@app.route("/", methods= ['POST'])
 def index():
+    username= request.form['username']
+    password= request.form['password']
+    verify= request.form['verify']
+    email= request.form['email']
+    name_error =''
+    pass_error= ''
+    ver_error= ''
+    email_error= ''
+
+    for i in username:
+        if i.isalpha == False:
+            name_error = 'Enter valid username'
+
+    
+    
+#if len(username) < 3 or len(username) > 20:
+        
+
+
+
+
+
+
+
+
+
     return render_template('index.html')
 
 
