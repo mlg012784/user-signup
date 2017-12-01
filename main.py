02af288b413b4aba34a1007d38a545e8a47bc311
Mlg012784 <mlg012784@yahoo.com>
2017-11-30 19:38:40 -0600

    finished user-signup

--- main.py
+++ main.py
@@ -19,25 +19,60 @@ def index():
     pass_error= ''
     ver_error= ''
     email_error= ''
-
-    for i in username:
-        if i.isalpha == False:
+    if len(username) < int(3) or len(username) > int(20):
+        username = ''
+        name_error = 'Enter valid username'
+    else:
+        if ' ' in username:
             name_error = 'Enter valid username'
+            username = ''
 
+    if len(password) < int(3) or len(password) > int(20):
+        pass_error = "That's not a valid password"
+        password = '' 
+    else:
+        if ' ' in password:
+            pass_error = "That's not a valid password"
+            password = ''
+    if password != verify:
+        ver_error = 'passwords do not match' 
+        verify= ''
+    while len(email) > int(0):
+        if len(email) < int(3) or len(email) > int(20):
+            email_error= 'Email not valid'
+            email= ''
     
+        else:
+            if not '@' in email:
+                email_error = 'Email not valid'
+                email= ''
+            else:
+                if not '.' in email:     
+                    email_error = 'Email not valid'
+                    email= ''
+                else:
+                    if ' ' in email:
+                        email_error= 'Email not valid'
+                        email = ''    
     
-#if len(username) < 3 or len(username) > 20:
+        if not email_error:
+            break
+    if not name_error and not pass_error and not ver_error and not email_error:  
         
+        return redirect('/welcome?username='  + username)     
+    else:
+         return render_template('index.html', name_error=
+            name_error, username=username, pass_error=pass_error, password=password,ver_error=ver_error, verify=verify, email_error=email_error, email=email)
+
+@app.route('/welcome')
+def welcome():
+    username =request.args.get('username')
+    return render_template('welcome.html', username = username)
 
 
 
 
 
-
-
-
-
-    return render_template('index.html')
 
 
 app.run()
